aa8aa8e891d7701e814439c4636caaa42f09c63a
btabur <webtasarim.0323@gmail.com>
2024-03-02 22:23:14 +0300

    created frontend/footer and pollicy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Policiy from './components/Policy'
 
 const App = () => {
   return (
    <main>
     <Header/>
+    <Policiy/>
+    <Footer/>
    </main>
   )
 }
