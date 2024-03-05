e4e41e74cd44c42613c6dc12c583d13fc532af30
Balı Tabur <webtasarim.0323@gmail.com>
2024-03-05 12:58:30 +0300

    created frontend/slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Policiy from './components/Policy'
+import Header from './components/Layout/Header/Header'
+import Footer from './components/Layout/Footer/Footer'
+import Policiy from './components/Layout/Policy/Policy'
+import Sliders from './components/Slider/Sliders'
 
 const App = () => {
   return (
    <main>
     <Header/>
+    <Sliders/>
     <Policiy/>
     <Footer/>
    </main>
