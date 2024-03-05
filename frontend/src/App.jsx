db4d8a7fc8e5e77b1ef659f030603c34b58c1415
Balı Tabur <webtasarim.0323@gmail.com>
2024-03-05 13:30:52 +0300

    frontend/ created products component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Footer from './components/Layout/Footer/Footer'
 import Policiy from './components/Layout/Policy/Policy'
 import Sliders from './components/Slider/Sliders'
 import Categories from './components/Categories/Categories'
+import Products from './components/Products/Products'
+import './App.css'
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
     <Header/>
     <Sliders/>
     <Categories/>
+    <Products/>
     <Policiy/>
     <Footer/>
    </main>
