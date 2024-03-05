da5ff50558551b4023f02c9eec95f664c08f152f
Balı Tabur <webtasarim.0323@gmail.com>
2024-03-05 13:09:56 +0300

    frontend/ created categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Layout/Header/Header'
 import Footer from './components/Layout/Footer/Footer'
 import Policiy from './components/Layout/Policy/Policy'
 import Sliders from './components/Slider/Sliders'
+import Categories from './components/Categories/Categories'
 
 const App = () => {
   return (
    <main>
     <Header/>
     <Sliders/>
+    <Categories/>
     <Policiy/>
     <Footer/>
    </main>
