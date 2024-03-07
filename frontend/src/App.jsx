39162c2fd4a3a8088a2a19716159b8fd3cc675af
Balı Tabur <webtasarim.0323@gmail.com>
2024-03-07 13:12:36 +0300

    frontend/ created auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import HomePage from './pages/HomePage'
 import ShopPage from './pages/ShopPage'
 import ContactPage from './pages/ContactPage'
+
+import AuthPage from './pages/AuthPage'
 import './App.css'
 
 const App = () => {
   return (
    <React.Fragment>
-    <ContactPage/>
+    <AuthPage/>
   
    
    </React.Fragment>
