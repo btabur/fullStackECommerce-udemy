f2aaf0d9961e156f31f31e432bc3f486b6a95db2
btabur <webtasarim.0323@gmail.com>
2024-03-28 12:03:05 +0300

    backend/ get a single category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -33,4 +33,24 @@ router.get("/",async(req,res)=> {
    }
 })
 
+// belirli bir kategoriyi getirme
+
+router.get("/:categoryId", async ( req,res)=> {
+
+    try {
+        const categoryId = req.params.categoryId;
+        try {
+            const category = await Category.findById(categoryId)
+            res.status(200).json(category)
+            
+        } catch (error) {
+            res.status(404).json({error:"Category not found"})
+        }
+
+    } catch (error) {
+        res.status(500).json({error:"Server Error"})
+    }
+    
+})
+
 module.exports= router;
