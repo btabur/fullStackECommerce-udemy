28468968a3d781c6e989320ad8b5d29097c91066
btabur <webtasarim.0323@gmail.com>
2024-04-01 12:37:24 +0300

    backend/Product Model/ created product model

---
+++ backend/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema (
+    {
+        text: {type:String, required:true},
+        rating: {type:Number,required:true},
+        user:{type:mongoose.Schema.Types.ObjectId, ref:"User", required:true}
+    }
+)
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {type: String,required: true},
+    img: [{type: String,required: true}], // array içerisinde tipi string olan birden fazla eleman gelebilir
+    reviews: [ReviewSchema],
+    description: {type: String,required: true},
+    colors:[{type: String,required: true}],
+    size:[{type: String,required: true}],
+    price: {
+        current: {type:Number, required:true},
+        discount: {type:Number}
+    },
+    category: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Category",
+        required:true
+    }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema)
+
+module.exports=Product;
