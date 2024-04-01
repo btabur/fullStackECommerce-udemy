b044abe31bf478806734c8e9ed1d5f103e0e3ff7
btabur <webtasarim.0323@gmail.com>
2024-04-01 11:56:02 +0300

    backend / User Model/Signing up with an existing email has been blocked

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,6 +11,12 @@ router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
+    const existingUser = await User.findOne({email});
+
+    if(existingUser) {
+        return res.status(400).json({error:"Email address is already registed"})
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await new User({
       username,
