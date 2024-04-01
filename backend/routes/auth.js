864ef9380cf0e0a3f5dc2443ae6d9a3444097dee
btabur <webtasarim.0323@gmail.com>
2024-04-01 11:51:11 +0300

    backend/User Model/User login passwords are encrypted and saved in the database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,28 +1,28 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const User = require("../models/User.js")
+const User = require("../models/User.js");
 
 const router = express.Router();
 
 //kullanıcı oluşturma ( create- register)
 
-router.post("/register" ,async(req,res)=> {
-    try {
-        const {username,email,password} = req.body;
-      const newUser =  await new User({username,email,password})
-      await newUser.save();
-        res.status(201).json(newUser)
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error:"server error"})
-        
-    }
-})
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "server error" });
+  }
+});
 
-
-
-
-
-module.exports =router;
+module.exports = router;
