24eb846716dee8c1de0cb2a28f7a08a226916827
btabur <webtasarim.0323@gmail.com>
2024-04-06 23:28:23 +0300

    frontend / get user info from api

--- frontend/src/pages/admin/AdminUserPage.jsx
+++ frontend/src/pages/admin/AdminUserPage.jsx
@@ -1,41 +1,70 @@
 import { Table } from 'antd';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const AdminUserPage = () => {
-    const dataSource = [
-        {
-          key: '1',
-          name: 'Mike',
-          age: 32,
-          address: '10 Downing Street',
-        },
-        {
-          key: '2',
-          name: 'John',
-          age: 42,
-          address: '10 Downing Street',
-        },
-      ];
-      
+
+    const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+    const [dataSource,setDataSource] = useState([]);
+    const [loading,setLoading] = useState(false);
+
+   
       const columns = [
         {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
+          title: 'Username',
+          dataIndex: 'username',
+          key: 'username',
         },
         {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email',
         },
         {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
+            title: 'Role',
+            dataIndex: 'role',
+            key: 'role',
+          },
+        {
+          title: 'Avatar',
+          dataIndex: 'avatar',
+          key: 'avatar',
+          render:(imgSrc)=> (
+            <img src={imgSrc} style={{width:'50px', borderRadius:'50%'}} />
+          )
         },
       ];
+
+      const fetchUser = async()=> {
+       
+        try {
+            setLoading(true)
+            const response = await fetch(`${apiUrl}/api/users`)
+          
+           if(response.ok){
+            const data = await response.json();
+            setDataSource(data)
+           }else {
+            message.error("Kullanıcılar getirilemedi ")
+           }
+
+        } catch (error) {
+            console.log("Giriş hatası",error);
+        }finally{
+            setLoading(false)
+        }
+        
+    }
+
+    useEffect(()=> {
+            fetchUser()
+    },[])
   return (
-   <Table dataSource={dataSource} columns={columns} />
+   <Table 
+   dataSource={dataSource}
+    columns={columns} 
+    rowKey={(record)=> record._id} 
+    loading={loading}/>
   )
 }
 
