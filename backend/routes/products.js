30d2868486f02a2416c0db7b9ac7005ba823de62
btabur <webtasarim.0323@gmail.com>
2024-05-08 12:47:15 +0300

    backend/ made product search route

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -92,4 +92,19 @@ router.delete("/:productId" , async (req,res)=> {
     }
 })
 
+// ürünlerin isme göre ara
+router.get("/search/:productName",async(req,res)=> {
+    try {
+        const productName = req.params.productName;
+        const products= await Product.find({
+            name:{$regex:productName , $options:"i"}
+            
+        })
+        res.status(200).json(products)
+        
+    } catch (error) {
+        res.status(500).json({error:"Server Error"})
+    }
+})
+
 module.exports= router;
